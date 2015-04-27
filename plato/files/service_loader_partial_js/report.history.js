935681424cce65745d4b069bed42ced6ca7007da
knalli <knallisworld@googlemail.com>
2015-04-27 22:35:46 +0200

    Snapshot Mo 27 Apr 2015 22:35:46 CEST

--- plato/files/service_loader_partial_js/report.history.js
+++
@@ -1,1 +0,0 @@
-__history = [{"date":"Mon, 27 Apr 2015 20:31:14 GMT","sloc":499,"lloc":155,"functions":29,"deliveredBugs":2.2601327186430447,"maintainability":69.88789610846392,"lintErrors":[],"difficulty":46.45454545454546}]
